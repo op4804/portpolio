97cafbb00736093781b98a3a0728b75a69964251
g0na <dlcndgh70@gmail.com>
2024-04-23 03:14:50 +0900

    movements, jump
    점프 진행 중

--- Scripts/CharacterManager.cs
+++ Scripts/CharacterManager.cs
@@ -4,20 +4,62 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+    [Header("Horizontal Movement Settings")]
+    private Rigidbody2D rb;
+    [SerializeField] private float walkSpeed = 1;
+    private float xAxis;
+
+    [Header("Ground Check Settings")]
+    private float jumpForce = 45;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.LeftArrow)) {
-            transform.Translate(-1,0,0);
+        GetInputs();
+        Move();
+        Jump();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
         }
-        else if (Input.GetKeyUp(KeyCode.RightArrow)) {
-            transform.Translate(1,0,0);
+        else
+        {
+            return false;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
         }
     }
 }
