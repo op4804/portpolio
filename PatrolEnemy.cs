c21067ec753b56277378721d51b2144be989bc97
op4804 <op4804@gmail.com>
2024-04-25 01:26:12 +0900

    add PatrolEnmey
    add Enemy scripts
    add PatrolEnemt scripts
    add Start and End for flatfomr(not working yet)
    move camera script file to scripts folder

---
+++ PatrolEnemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEnemy : Enemy
+{
+    // 시작 지점
+    [Header("Patrol set")]
+    private float startPosition;
+    private float patrolDirection;
+    [SerializeField] private float patrolWidth;
+    [SerializeField] private float patrolStep;
+    [SerializeField] private float patrolStepTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = this.transform.position.x;
+        patrolDirection = -1;
+        StartCoroutine(Patrol());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Patrol()
+    {
+        while (true)
+        {
+            if (patrolDirection == -1 && this.transform.position.x < startPosition - patrolWidth / 2)
+            {
+                patrolDirection = 1;
+            }
+            else if(patrolDirection == 1 && this.transform.position.x > startPosition + patrolWidth / 2)
+            {
+                patrolDirection = -1;
+            }
+            this.transform.Translate(new Vector3(patrolStep * patrolDirection, 0, 0));
+            yield return new WaitForSeconds(patrolStepTime);
+        }
+
+    }
+
+}
