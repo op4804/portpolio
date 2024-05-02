009c05289470c9aaed763a578ff38cf3310f17cc
gona <118372076+g0na@users.noreply.github.com>
2024-05-02 19:36:07 +0900

    Update Recoil, Hp, Layers
    Change recoil position, platform's Layers & Add invulnerable time, Hp

--- Scripts/CharacterManager.cs
+++ Scripts/CharacterManager.cs
@@ -5,12 +5,12 @@ using UnityEngine.Playables;
 
 public class CharacterManager : MonoBehaviour
 {
-
     [Header("Horizontal Movement Settings")]
     [SerializeField] private float walkSpeed = 1;
     
     [Header("Verticla Movement Settings")]
     private float gravity;
+
 
 
     [Header("Ground Check Settings")]
@@ -20,14 +20,31 @@ public class CharacterManager : MonoBehaviour
     [SerializeField] private float groundCheckX = 0.5f;
     [SerializeField] private LayerMask whatIsGround;
 
-    [Header("Recoil")]
+
+
+    [Header("Recoil Settings")]
     // 이동에 걸리는 시간 (초)
     [SerializeField] public float moveDuration = 0.1f;
     
     [SerializeField] private Animator anim;
 
+
+
+    [Header("HP & Attack Damage")]
+    [SerializeField] int hp = 10;
+    [SerializeField] int knockbackSpeed = 10;
+    // for enemy attack
+    [SerializeField] float damage = 1;
+
+
+    // Noraml Variables
     private Rigidbody2D rb;
     PlayerStateList pState;
+    SpriteRenderer sr;
+    bool isHurt;
+    bool isKnockback;
+    Color halfA = new Color(1, 1, 1, 0.5f);
+    Color fullA = new Color(1, 1, 1, 1);
     private float xAxis;
     bool attack = false;
     float timeBetweenAttack, timeSinceAttack;
@@ -35,11 +52,6 @@ public class CharacterManager : MonoBehaviour
     private Vector2 targetPosition;
     public static CharacterManager Instance;
 
-
-
-    // for enemy attack
-
-    [SerializeField] float damage = 1;
 
 
 
@@ -60,6 +72,7 @@ public class CharacterManager : MonoBehaviour
     {
         pState = GetComponent<PlayerStateList>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -72,6 +85,7 @@ public class CharacterManager : MonoBehaviour
         Attack();
     }
 
+    // Recoil when Player attack with weapon
     private void OnTriggerEnter2D(Collider2D _other)
     {
         if (_other.tag == "Enemy")
@@ -101,25 +115,29 @@ public class CharacterManager : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D _other)
+
+    // Recoil when Player gets hit with Enemy
+    private void OnCollisionStay2D(Collision2D _other)
     {
         if (_other.gameObject.tag == "Enemy")
         {
             Debug.Log("Body Hit!");
 
-            if (transform.position.x - _other.transform.position.x > 0) 
-            {
-                // 목표 위치 설정
-                targetPosition = transform.position + new Vector3(2, 2, 0);
-            }
-            else if (transform.position.x - _other.transform.position.x < 0)
-            {
-                // 목표 위치 설정
-                targetPosition = transform.position + new Vector3(-2, 2, 0);
-            }
+            Hurt(1, _other.transform.position);
 
-            // Coroutine 시작
-            StartCoroutine(MoveOverTime());
+            // if (transform.position.x - _other.transform.position.x > 0) 
+            // {
+            //     // 목표 위치 설정
+            //     targetPosition = transform.position + new Vector3(2, 2, 0);
+            // }
+            // else if (transform.position.x - _other.transform.position.x < 0)
+            // {
+            //     // 목표 위치 설정
+            //     targetPosition = transform.position + new Vector3(-2, 2, 0);
+            // }
+
+            // // Coroutine 시작
+            // StartCoroutine(MoveOverTime());
         }
     }
 
@@ -208,5 +226,72 @@ public class CharacterManager : MonoBehaviour
             timeSinceAttack = 0;
             anim.SetTrigger("Attack");
         }
+    }
+
+    // Player gets damage function
+    public void Hurt(int damage, Vector2 pos)
+    {
+        if (!isHurt)
+        {
+            isHurt = true;
+            hp = hp - damage;
+            if (hp < 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                float x = transform.position.x - pos.x;
+                if (x < 0)
+                  x = 1;
+                else
+                  x = -1;
+
+                StartCoroutine(Knockback(x));
+                StartCoroutine(Invulnerable());
+                StartCoroutine(AlphaBlink());
+            }
+        }
+    }
+
+    // Recoil Function
+    private IEnumerator Knockback(float dir)
+    {
+        isKnockback = true;
+        float ctime = 0;
+        while(ctime < 0.2f)
+        {
+            if (transform.rotation.y == 0)
+            {
+                transform.Translate(Vector2.left * knockbackSpeed * Time.deltaTime * dir);
+            }
+            else
+            {
+                transform.Translate(Vector2.left * knockbackSpeed * Time.deltaTime * -1f * dir);
+            }
+
+            ctime += Time.deltaTime;
+            yield return null;
+        }
+        isKnockback = false;
+    }
+
+    // Player blinks while player's status is invulnerable
+    private IEnumerator AlphaBlink()
+    {
+        while(isHurt)
+        {
+            yield return new WaitForSeconds(0.1f);
+            sr.color = halfA;
+            yield return new WaitForSeconds(0.1f);
+            sr.color = fullA;
+        }
+    }
+    
+    // Turning player's status to invulnerable for a second
+    private IEnumerator Invulnerable()
+    {
+        yield return new WaitForSeconds(1f);
+        isHurt = false;
     }
 }
